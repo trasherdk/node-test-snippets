a4f38d7066fddd14fdb1e7922611642a8d62c486
TrasherDK <trasherdk@fumlersoft.dk>
2022-06-24 14:01:27 +0200

    ccxws: Filter specific asset

--- ccxws/src/minimal.js
+++ ccxws/src/minimal.js
@@ -1,17 +1,19 @@
-import ccxt from 'ccxt';
-import { KrakenClient } from 'ccxws';
+import ccxt from 'ccxt'
+import { KrakenClient } from 'ccxws'
 
 async function processTradeData (trade) {
   console.log(trade)
 }
 
-async function main (exchange = new ccxt.kraken()) {
-  const ws = new KrakenClient();
-  ws.on('trade', (trade) => processTradeData(trade));
-  const krakenMarkets = (await exchange.fetchMarkets()).map(x => ({ id: x.id, base: x.base, quote: x.quote }));
+async function main (ticker, exchange = new ccxt.kraken()) {
+  const ws = new KrakenClient()
+  ws.on('trade', (trade) => processTradeData(trade))
+  const krakenMarkets = (await exchange.fetchMarkets())
+    .filter(x => x.base === ticker)
+    .map(x => ({ id: x.id, base: x.base, quote: x.quote }))
   for (const market of krakenMarkets) {
     ws.subscribeTrades(market)
   }
 }
 
-main();
+main('XMR')
