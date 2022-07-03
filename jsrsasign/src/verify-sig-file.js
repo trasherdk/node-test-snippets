d162de28062fd05752b368d72eb7adccebd7016d
TrasherDK <trasherdk@fumlersoft.dk>
2022-07-03 07:37:53 +0200

    jsrsasign: verify by signature file - renamed from verify-sig.js

--- jsrsasign/src/verify-sig-file.js
+++ jsrsasign/src/verify-sig-file.js
@@ -1,14 +1,14 @@
+import { readFileSync } from 'fs'
 import rs from 'jsrsasign'
-import rsu from 'jsrsasign-util'
 import path, { resolve } from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const pubpem = rsu.readFileUTF8(resolve(__dirname, '../ca/server-crt.pem'))
-const sighex = rsu.readFileHexByBin(resolve(__dirname, '../temp/message.txt.sig.bin'))
-const msghex = rsu.readFileHexByBin(resolve(__dirname, '../message.txt'))
+const pubpem = readFileSync(resolve(__dirname, '../ca/server-crt.pem')).toString('utf8')
+const sighex = readFileSync(resolve(__dirname, '../temp/message.txt.sig.bin')).toString('hex')
+const msghex = readFileSync(resolve(__dirname, '../message.txt')).toString('hex')
 
 const sig = new rs.KJUR.crypto.Signature({ alg: 'SHA256withRSAandMGF1' })
 sig.init(pubpem)
