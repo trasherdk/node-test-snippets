98f3ee84a79437a7eeb350f62c6e78f8ed3100a8
TrasherDK <trasherdk@fumlersoft.dk>
2022-07-03 07:30:59 +0200

    jsrsasign: sign-verify text/hex

---
+++ jsrsasign/src/sign-verify-hex.js
@@ -0,0 +1,27 @@
+import { readFileSync } from 'fs'
+import jsrsasign from 'jsrsasign'
+import path, { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const Private = readFileSync(resolve(__dirname, '../ca/server-key.pem')).toString('utf8')
+const Public = readFileSync(resolve(__dirname, '../ca/server-crt.pem')).toString('utf8')
+const Payload = readFileSync(resolve(__dirname, '../message.txt'))
+const payload = Payload.toString('hex')
+
+// sign
+const sig = new jsrsasign.KJUR.crypto.Signature({ "alg": "SHA256withRSAandMGF1" });
+const prikey = jsrsasign.KEYUTIL.getKey(Private);
+
+sig.init(prikey);
+sig.updateHex(payload);
+let sign = sig.sign();
+
+// verify text
+const sig2 = new jsrsasign.KJUR.crypto.Signature({ "alg": "SHA256withRSAandMGF1" });
+const pubkey = jsrsasign.KEYUTIL.getKey(Public);
+sig2.init(pubkey);
+sig2.updateHex(payload);
+console.log('Verify HEX:', sig2.verify(sign));// true
