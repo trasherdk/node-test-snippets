fee4fc31f972d6ae68d1099d6ffdfa4ed74932b5
TrasherDK <trasherdk@fumlersoft.dk>
2022-07-09 09:27:54 +0200

    deps: bump ccxt, zeromq to 6.x beta - refactor zmq test to 6,x

--- monero/src/node-zmq-grampy.js
+++ monero/src/node-zmq-grampy.js
@@ -4,32 +4,52 @@ import { config } from 'dotenv'
 config()
 const { MONERO_HOST, MONERO_ZMQ_PORT } = process.env
 
-const sock = zmq.socket('sub');
+const sock = new zmq.Subscriber();
 
-sock.on("bind", event => {
-  console.log(`Socket bound to ${event.address}`)
-  // ...
-})
-
-sock.on('message', (message) => {
+const onMessage = (message) => {
   let messageClean = Buffer.from(message).toString()
   messageClean = messageClean.slice(messageClean.indexOf(':') + 1)
   console.log(messageClean)
   // console.log(message2.json-minimal-txpool_add);
-})
+}
 
-sock.on('connect', (event) => {
-  console.log('Subscriber connected to %s:%s', MONERO_HOST, MONERO_ZMQ_PORT)
+const onConnect = (event) => {
+  console.log('Connected to %s', event.address)
 
-})
+  // sock.subscribe('json-minimal-txpool_add')
+  // sock.subscribe('json')
+}
+
+const onHandshake = (event) => {
+  console.log('Handshake from %s', event.address)
+
+  // sock.subscribe('json-minimal-txpool_add')
+  sock.subscribe('json')
+}
 
 // Port should be your monerod --zmq-pub port
-console.log('Subscriber connecting to %s:%s', MONERO_HOST, MONERO_ZMQ_PORT)
+console.log('Connecting to %s:%s', MONERO_HOST, MONERO_ZMQ_PORT)
 
 sock.connect(`tcp://${MONERO_HOST}:${MONERO_ZMQ_PORT}`);
 
-sock.subscribe('json-minimal-txpool_add');
-
+for await (Event of sock.events) {
+  switch (Event.type) {
+    case "bind":
+      console.log(`Socket bound to ${Event.address}`)
+      break
+    case "connect":
+      onConnect(Event)
+      break
+    case "handshake":
+      onHandshake(Event)
+      break
+    case "message":
+      onMessage(Event)
+      break
+    default:
+      console.log('Event %s from %s', Event.type, Event.address)
+  }
+}
 /*
 export interface JsonMinimalTxpoolAdd {
   id: string //hash?
