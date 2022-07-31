b17417d60435105557fe7778004d00f9174d8b72
TrasherDK <trasherdk@fumlersoft.dk>
2022-07-31 08:18:38 +0200

    Add `worker/threads/src/auth`

---
+++ worker/threads/src/auth/auth.js
@@ -0,0 +1,14 @@
+// workers/auth.js
+// import sha256 from "js-sha256"
+import crypto from "crypto"
+import { expose } from "threads/worker"
+
+const sha256 = (data) => {
+  return crypto.createHash('sha256').update(data).digest('base64')
+}
+
+expose({
+  hashPassword (password, salt) {
+    return sha256(password + salt)
+  }
+})
