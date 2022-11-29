73c268c75994be7f305b8cfa3d93364fb3ce4d0c
TrasherDK <trasherdk@fumlersoft.dk>
2022-11-29 03:21:52 +0100

    `monero/sh/open_wallet.sh` source `.env.stagenet`

--- monero/sh/open_wallet.sh
+++ monero/sh/open_wallet.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-curl -k -vv https://86.48.96.142:38091/json_rpc \
+ROOT=$(realpath "$(dirname $0)/../..")
+
+source ${ROOT}/.env.stagenet
+
+curl -k -vv https://${MONERO_WALLET_RPC_HOST}:${MONERO_WALLET_RPC_PORT}/json_rpc \
   --tlsv1.2 --tls-max 1.2 \
   --digest -u 'rpc_user:abc123' \
   -d '{"jsonrpc":"2.0","id":"0","method":"open_wallet","params":{"filename":"userwallet","password":"5l1nger"}}' \
