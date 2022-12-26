f272ab0943d0a1653a56622842616d9deb3a9e64
TrasherDK <trasherdk@fumlersoft.dk>
2022-12-26 12:36:30 +0100

    tardis-node: example.js

--- tardis-node/src/real-time.js
+++ tardis-node/src/real-time.js
@@ -4,18 +4,19 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const tardisPath = resolve(__dirname, './../../node_modules/tardis-node/dist/index.js')
+const tardisPath = resolve(__dirname, '../../node_modules/tardis-node/dist/index.js')
 console.log('Path:', tardisPath)
-import { replayNormalized, streamNormalized, normalizeTrades, compute, computeTradeBars } from './../../node_modules/tardis-node/dist/index.js'
-
+// import { replayNormalized, streamNormalized, normalizeTrades, compute, computeTradeBars } from '../../node_modules/tardis-node/dist/index.js'
+import { tardis, streamNormalized } from 'tardis-node'
+// const { streamNormalized } = tardis
 // import * as tardis from 'tardis-node'
 // const { replayNormalized, streamNormalized, normalizeTrades, compute, computeTradeBars } = tardis
 
 const messages = streamNormalized({
-  exchange: 'bitmex',
+  exchange: 'kraken',
   filters: [
-    { channel: 'trade', symbols: ['XBTUSD'] },
-    { channel: 'orderBookL2', symbols: ['XBTUSD'] }
+    { channel: 'trade', symbols: ['XMREUR'] },
+    { channel: 'orderBookL2', symbols: ['XMREUR'] }
   ]
 })
 
