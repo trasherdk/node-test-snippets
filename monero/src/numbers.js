f73caf1d24050d284c0abd7dddef9c465eb790e6
trasherdk <trasherdk@gmail.com>
2023-08-18 07:47:35 +0200

    Monero: fix number conversion, trailing zeroes

--- monero/src/numbers.js
+++ monero/src/numbers.js
@@ -2,12 +2,12 @@ import BigNumber from 'bignumber.js';
 
 const oneXMR = new BigNumber('1000000000000')
 
-const xmrPiconero1 = new BigNumber('12345678000'); console.log(xmrPiconero1, xmrPiconero1.dividedBy(oneXMR))
-const xmrPiconero2 = new BigNumber('12345678000'); console.log(xmrPiconero2, xmrPiconero2.dividedBy(oneXMR))
-const xmrPiconeroResult = xmrPiconero1.plus(xmrPiconero2); console.log(xmrPiconeroResult, xmrPiconeroResult.dividedBy(oneXMR))
+const xmrPiconero1 = new BigNumber('12345678000'); console.log('Piconeros %s - XMR - %s', xmrPiconero1, xmrPiconero1.dividedBy(oneXMR))
+const xmrPiconero2 = new BigNumber('12345678000'); console.log('Piconeros %s - XMR - %s', xmrPiconero2, xmrPiconero2.dividedBy(oneXMR))
+const xmrPiconeroResult = xmrPiconero1.plus(xmrPiconero2); console.log('Piconeros %s - XMR - %s', xmrPiconeroResult, xmrPiconeroResult.dividedBy(oneXMR))
 
-const [xmr, picos] = xmrPiconeroResult.dividedBy(oneXMR).toString().split('.')
-console.log('XMR %s : Picos %s', xmr, picos)
+const [xmr, picos] = xmrPiconeroResult.dividedBy(oneXMR).toFixed(12).toString().split('.')
+console.log('Piconeros %s - XMR - %s', +picos, xmr)
 
 const displayValue = xmr + '.' + picos.slice(0, 6)
 console.log('Display:', displayValue)
