d6b0f47e4390e61911a7afac9749f54e580208d3
trasherdk <trasherdk@gmail.com>
2023-09-15 06:05:40 +0200

    map2json: output format

--- map-jason/src/map2json.js
+++ map-jason/src/map2json.js
@@ -1,6 +1,8 @@
 const map1 = new Map([['str', new Array(6).fill(0.0)]])
 const jsonFromMap = JSON.stringify(Object.fromEntries(map1))
+console.log('jsonFromMap', jsonFromMap)
 
 const map2 = new Map(Object.entries(JSON.parse(jsonFromMap)))
 
-console.log(map1, '\n', map2)
+console.log('map1:', map1)
+console.log('map2:', map2)
