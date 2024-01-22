931212e39a1ffdba38d8830290055b4abc66e7ea
trasherdk <trasherdk@gmail.com>
2024-01-22 13:08:50 +0100

    Native fetch need undici Agent for TLS :(

--- node-fetch/fetch-head.js
+++ node-fetch/fetch-head.js
@@ -4,15 +4,35 @@
  * or
  * node --use-openssl-ca node-fetch/fetch-head.js
  */
+import { Agent } from "undici"
+import { readFileSync } from 'fs'
+
+const ca = readFileSync("certs/ca.crt")
+const cert = readFileSync("certs/ghost-node.fumlersoft.dk.crt")
+const key = readFileSync("certs/ghost-node.fumlersoft.dk.key")
+
+const agent = new Agent({
+  connect: {
+    cert: cert,
+    key: key,
+    ca: ca
+  }
+})
 
 const fetch1 = async () => {
-  const { ok } = await fetch('https://mydns-asus.fumlersoft.dk/nope.png', { method: 'HEAD' })
-  console.log(ok)
+  const res = await fetch('https://ns1.fumlersoft.dk/earth-64x64.png', {
+    method: 'HEAD',
+    dispatcher: agent
+  })
+  console.log('fetch1:', res)
 }
 
 const fetch2 = async () => {
-  const { ok } = await fetch('https://mydns-asus.fumlersoft.dk/earth-64x64.png', { method: 'HEAD' })
-  console.log(ok)
+  const res = await fetch('https://ns2.fumlersoft.dk/earth-64x64.png', {
+    method: 'HEAD',
+    dispatcher: agent
+  })
+  console.log('fetch2:', res)
 }
 
 await fetch1()
